38c3886cdd9e3a1bc2c46d0ebd257adad53ad7f3
Vika Chudnovskai <viktoria.chudnovskay@yandex.ru>
2024-05-04 13:51:00 +0300

    secont comin

---
+++ src/main/kotlin/animal.java
@@ -0,0 +1,16 @@
+abstract class animal {
+    abstract val image: String
+    abstract val food: String
+    abstract val habitat: String
+    var hunger = 18
+    abstract fun makeNoise()
+    abstract fun eat()
+    open fun roam(){
+        println("Животное передвигается")
+        1
+        open fun sleep() {
+            println("Животное спит")
+        }
+
+
+}
---
+++ src/main/kotlin/zadani.kt
@@ -0,0 +1,21 @@
+package org.example
+
+abstract class Appliance {
+    var pluggedIn = true
+    abstract val color: String
+    abstract fun consumPower()
+}
+
+class CoffeeMaker : Appliance() {
+    override val color = ""
+    var coffeeLeft = false
+    override fun consumPower(){
+        println("Consuming power")
+    }
+    fun fillWithWater() {
+        println("Fill with water")
+    }
+    fun makeCoffee() {
+        println("Make the coffee")
+    }
+}
